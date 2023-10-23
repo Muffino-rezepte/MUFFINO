7d69099474f42f267ed0c73b2584bb5990665f04
Muffino-rezepte <145963513+Muffino-rezepte@users.noreply.github.com>
2023-10-23 06:56:21 +0000

    modified:   index.html 	new file:   script.js 	modified:   style.css

---
+++ script.js
@@ -0,0 +1,67 @@
+let dropvar = 0;
+function burger_openmenu(){
+    if(dropvar == 0){
+        var drop = document.getElementById('burger_dropdown');
+    drop.style.opacity = 1;
+    drop.style.height = "200px";
+    drop.style.backgroundColor = "rgb(226, 226, 226)";
+    dropvar = 1;
+
+    var line1 = document.getElementById('burger_line1');
+    line1.style.transform = "rotate(45deg)";
+    line1.style.top = "11px";
+    line1.style.transition = "transform 0.5s, top 0.5s";
+
+    var line2 = document.getElementById('burger_line2');
+    line2.style.opacity = "0"
+    line2.style.transition = "opacity 0s";
+
+    var line3 = document.getElementById('burger_line3');
+    line3.style.transform = "rotate(-45deg)";
+    line3.style.top = "11px";
+    line3.style.transition = "transform 0.5s, top 0.5s";
+
+    var burgercon = document.getElementById('burger_content');
+    burgercon.style.opacity = 1;
+    burgercon.style.transition = "opacity 0.5s";
+
+    var cr = document.getElementById('copyright');
+    cr.style.opacity = 0.7;
+    cr.style.transition = "opacity 0.5s";
+
+    }else{
+        var drop = document.getElementById('burger_dropdown');
+        drop.style.height = "0px";
+        drop.style.opacity = 0;
+        drop.style.transition = "height 0.5s, opacity 0.5s";
+    dropvar = 0;
+
+    var line1 = document.getElementById('burger_line1');
+    line1.style.transform = "rotate(0deg)";
+    line1.style.top = "0px";
+    line1.style.right = "0px";
+    line1.style.transition = "transform 0.5s, top 0.5s";
+
+    var line2 = document.getElementById('burger_line2');
+    line2.style.opacity = "1"
+    line2.style.transition = "opacity 0.5s";
+
+    var line3 = document.getElementById('burger_line3');
+    line3.style.transform = "rotate(0deg)";
+    line3.style.top = "20px";
+    line3.style.right = "0px";
+    line3.style.transition = "transform 0.5s, top 0.5s";
+    
+    var burgercon = document.getElementById('burger_content');
+    burgercon.style.opacity = 0;
+    burgercon.style.transition = "opacity 0.5s";
+
+    var cr = document.getElementById('copyright');
+    cr.style.opacity = 0;
+    cr.style.transition = "opacity 0.3s";
+
+    }
+
+
+
+}
